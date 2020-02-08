845b84147587a82aae0c7d912633db95792f0a89
hota1024 <hotalog@hota1024.com>
2020-02-08 20:19:50 +0900

    🏷️ Update ContentParseTokenType

--- tests/CommandParser.test.ts
+++ tests/CommandParser.test.ts
@@ -9,4 +9,15 @@ describe('ContentParser test', () => {
     expect(tokens[0].commandName).toBe('help')
     expect(tokens[1].type).toBe(ContentParseTokenType.CommandCallEnd)
   })
+
+  test('Parse command call with string argument', () => {
+    const parser = new ContentParser()
+    const tokens = parser.parse('reverse "Hello world"')
+
+    expect(tokens[0].type).toBe(ContentParseTokenType.CommandCallStart)
+    expect(tokens[0].commandName).toBe('reverse')
+    expect(tokens[1].type).toBe(ContentParseTokenType.StringArgument)
+    expect(tokens[1].stringValue).toBe('Hello world')
+    expect(tokens[2].type).toBe(ContentParseTokenType.CommandCallEnd)
+  })
 })
