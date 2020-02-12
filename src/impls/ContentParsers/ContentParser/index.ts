70aa2b9677a65e70bf670d00fb12859f25d10257
hota1024 <hotalog@hota1024.com>
2020-02-12 23:54:16 +0900

    🏷️Update ContentParser class

--- src/impls/ContentParsers/ContentParser/index.ts
+++ src/impls/ContentParsers/ContentParser/index.ts
@@ -2,21 +2,227 @@ import { ContentParserInterface } from '../../../interfaces/ContentParser'
 import { ContentParseTokenType, ContentParseToken } from '../../../types'
 
 /*
+ * ContentParserState enum.
+ */
+export enum ContentParserState {
+  CommandName,
+  Argument
+}
+
+/**
+ * ContentParser options type.
+ */
+export type ContentParserOptions = {
+  pipelineChars: string[]
+  escapeChars: string[]
+  stringChars: string[]
+  separatorChars: string[]
+  trueStrings: string[]
+  falseStrings: string[]
+}
+
+/*
  * ContentParser class.
  */
 export class ContentParser implements ContentParserInterface {
+  /**
+   * ContentParser defualt options.
+   */
+  static defaultOptions: ContentParserOptions = {
+    pipelineChars: ['>'],
+    escapeChars: ['\\'],
+    stringChars: [`"`, `'`],
+    separatorChars: [' '],
+    trueStrings: ['true', 'on'],
+    falseStrings: ['false', 'off']
+  }
+
+  /**
+   * Options.
+   */
+  options: ContentParserOptions
+
+  /**
+   * Constructor.
+   *
+   * @param options
+   */
+  constructor(
+    options: Partial<ContentParserOptions> = ContentParser.defaultOptions
+  ) {
+    this.options = {
+      ...ContentParser.defaultOptions,
+      ...options
+    }
+  }
+
   /**
    * Parse content.
    *
    * @param content
    */
   parse(content: string) {
-    return [
-      this.createToken(ContentParseTokenType.CommandCallStart, {
-        commandName: 'help'
-      }),
-      this.createToken(ContentParseTokenType.CommandCallEnd)
-    ]
+    const tokens = this.parseContent(content)
+
+    tokens.unshift(this.createToken(ContentParseTokenType.ResultStart))
+    tokens.push(this.createToken(ContentParseTokenType.ResultEnd))
+
+    return tokens
+  }
+
+  /**
+   * Parse content.
+   *
+   * @param content
+   */
+  private parseContent(content: string) {
+    let state = ContentParserState.CommandName
+    let isEscape = false
+    let isString = false
+    let buffer = ''
+
+    const tokens: ContentParseToken[] = []
+    const isState = (s: ContentParserState) => s === state
+    const length = content.length
+
+    for (let i = 0; i < length; ++i) {
+      const char = content[i]
+      let escapeCancled = false
+      const isEnd = i === length - 1
+      const isSeparator = this.isSeparatorChar(char)
+
+      if (!isSeparator || isEscape || isString) {
+        // Tokens.
+        if (this.isStringChar(char) && !isEscape) {
+          isString = !isString
+        } else if (this.isEscapeChar(char) && !isEscape) {
+          isEscape = true
+        } else if (this.isPipelineChar(char) && !isEscape) {
+          tokens.push(this.createToken(ContentParseTokenType.CommandCallEnd))
+          tokens.push(this.createToken(ContentParseTokenType.Pipeline))
+          state = ContentParserState.CommandName
+        } else if (!(isSeparator && isState(ContentParserState.CommandName))) {
+          buffer += char
+          isEscape = false
+          escapeCancled = true
+        }
+      }
+
+      if (isEnd || (isSeparator && !isEscape && !isString && !escapeCancled)) {
+        // States.
+        if (isState(ContentParserState.CommandName) && buffer !== '') {
+          tokens.push(
+            this.createToken(ContentParseTokenType.CommandCallStart, {
+              commandName: buffer
+            })
+          )
+
+          state = ContentParserState.Argument
+        } else if (isState(ContentParserState.Argument)) {
+          tokens.push(this.parseArgument(buffer))
+        }
+
+        if (isEnd) {
+          tokens.push(this.createToken(ContentParseTokenType.CommandCallEnd))
+        }
+
+        buffer = ''
+        isEscape = false
+      }
+    }
+
+    return tokens
+  }
+
+  /**
+   * Parse argument and returns token.
+   *
+   * @param value
+   */
+  private parseArgument(value: string) {
+    const numberValue = Number(value)
+
+    if (!isNaN(numberValue)) {
+      return this.createToken(ContentParseTokenType.NumberArgument, {
+        numberValue
+      })
+    } else if (this.isTrueString(value)) {
+      return this.createToken(ContentParseTokenType.BooleanArgument, {
+        booleanValue: true
+      })
+    } else if (this.isFalseString(value)) {
+      return this.createToken(ContentParseTokenType.BooleanArgument, {
+        booleanValue: false
+      })
+    } else {
+      return this.createToken(ContentParseTokenType.StringArgument, {
+        stringValue: value
+      })
+    }
+  }
+
+  /**
+   * Returns whether char is included in chars.
+   *
+   * @param chars
+   * @param char
+   */
+  private checkContain(chars: string[], char: string) {
+    return chars.findIndex(c => c === char) !== -1
+  }
+
+  /**
+   * Returns whether is pipeline char.
+   *
+   * @param char
+   */
+  private isPipelineChar(char: string) {
+    return this.checkContain(this.options.pipelineChars, char)
+  }
+
+  /**
+   * Returns whether is escape char.
+   *
+   * @param char
+   */
+  private isEscapeChar(char: string) {
+    return this.checkContain(this.options.escapeChars, char)
+  }
+
+  /**
+   * Returns whether is string char.
+   *
+   * @param char
+   */
+  private isStringChar(char: string) {
+    return this.checkContain(this.options.stringChars, char)
+  }
+
+  /**
+   * Returns whether is separator char.
+   *
+   * @param char
+   */
+  private isSeparatorChar(char: string) {
+    return this.checkContain(this.options.separatorChars, char)
+  }
+
+  /**
+   * Returns whether is true string.
+   *
+   * @param string
+   */
+  private isTrueString(string: string) {
+    return this.checkContain(this.options.trueStrings, string)
+  }
+
+  /**
+   * Returns whether is false string.
+   *
+   * @param string
+   */
+  private isFalseString(string: string) {
+    return this.checkContain(this.options.falseStrings, string)
   }
 
   /**
