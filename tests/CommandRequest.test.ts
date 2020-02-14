3853ad583570f6fc1dca36f0eb70a8da3676cbdd
hota1024 <hotalog@hota1024.com>
2020-02-14 14:49:53 +0900

    ✅ Update CommandRequest.test.ts

--- tests/CommandRequest.test.ts
+++ tests/CommandRequest.test.ts
@@ -3,15 +3,23 @@ import { CommandRequest } from '../src'
 describe('CommandRequest test', () => {
   test('Get arguments', () => {
     const request = new CommandRequest({
-      args: {
-        text: 'Hello world',
-        number: 10,
-        boolean: true
-      }
+      args: ['Hello world', 10, true]
     })
 
-    expect(request.get<string>('text')).toBe('Hello world')
-    expect(request.get<number>('number')).toBe(10)
-    expect(request.get<boolean>('boolean')).toBe(true)
+    expect(request.get<string>(0)).toBe('Hello world')
+    expect(request.get<number>(1)).toBe(10)
+    expect(request.get<boolean>(2)).toBe(true)
+  })
+
+  test('Get all arguments', () => {
+    const request = new CommandRequest({
+      args: ['Hello world', 10, true]
+    })
+
+    const [text, number, boolean] = request.all<[string, number, boolean]>()
+
+    expect(text).toBe('Hello world')
+    expect(number).toBe(10)
+    expect(boolean).toBe(true)
   })
 })
