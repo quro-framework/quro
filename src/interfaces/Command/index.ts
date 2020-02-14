64d0b8ae3a580fa2c426d89104ea5309aa3981d9
hota1024 <hotalog@hota1024.com>
2020-02-14 23:40:15 +0900

    🏷️ Add CommandInterface

---
+++ src/interfaces/Command/index.ts
@@ -0,0 +1,28 @@
+import { PromiseOr } from '../../types'
+import { CommandResponseInterface } from '../CommandResponse'
+import { PipeNextInterface } from '../PipeNext'
+
+/*
+ * CommandInterface.
+ */
+export interface CommandInterface {
+  /**
+   * Call on mounted.
+   */
+  onMount(): PromiseOr
+
+  /**
+   * Call on handled.
+   */
+  onHandle(): Generator<
+    PromiseOr<CommandResponseInterface>,
+    PromiseOr<CommandResponseInterface | void>
+  >
+
+  /**
+   * Call on pipe.
+   *
+   * @param next
+   */
+  onPipe(next: PipeNextInterface): PipeNextInterface
+}
