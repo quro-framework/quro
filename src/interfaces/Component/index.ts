72d1396d898b305c593a0ba858cb9e86f6530cbf
hota1024 <hotalog@hota1024.com>
2020-02-16 22:48:36 +0900

    🏷️ Add ConmponentInterface

---
+++ src/interfaces/Component/index.ts
@@ -0,0 +1,11 @@
+import { ContextInterface } from '../Context'
+
+/*
+ * ComponentInterface.
+ */
+export interface ComponentInterface {
+  /**
+   * Runtime context.
+   */
+  readonly context: ContextInterface
+}
