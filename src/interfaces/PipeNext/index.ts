28de6c7241468502e71daf3799909863dd2b6cd5
hota1024 <hotalog@hota1024.com>
2020-02-17 23:44:45 +0900

    🏷️ Update PipeNextInterface

--- src/interfaces/PipeNext/index.ts
+++ src/interfaces/PipeNext/index.ts
@@ -1,9 +1,15 @@
 import { RequestArgs } from '../../types'
+import { CommandRequestInterface } from '../CommandRequest'
 
 /*
  * PipeNextInterface.
  */
 export interface PipeNextInterface {
+  /**
+   * Command request.
+   */
+  readonly request: CommandRequestInterface
+
   /**
    * Set append args.
    *
