4681cad1113c95afac2ca892ad0862133f41653b
hota1024 <hotalog@hota1024.com>
2020-02-17 23:43:30 +0900

    🏷️ Update CommandRequestInterface

--- src/interfaces/CommandRequest/index.ts
+++ src/interfaces/CommandRequest/index.ts
@@ -1,5 +1,6 @@
 import { RequestArgs } from '../../types/RequestArgs'
 import { Message } from '../..'
+import { PipeNextInterface } from '../PipeNext'
 
 /*
  * CommandRequest interface.
@@ -11,9 +12,24 @@ export interface CommandRequestInterface {
   readonly message: Message
 
   /**
+   * Wheather call as pipe.
+   */
+  readonly isPipe: boolean
+
+  /**
+   * Pipe routes.
+   */
+  readonly pipeRoutes: PipeNextInterface[]
+
+  /**
    * Get argument by index.
    *
    * @param index
    */
   get<T extends RequestArgs[0]>(index: number): T
+
+  /**
+   * Get all arguments.
+   */
+  get<T extends RequestArgs>(): T
 }
