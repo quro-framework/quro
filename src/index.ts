bfcf59eb9b04ff383b8e6789aad122e04ca86fed
hota1024 <hotalog@hota1024.com>
2020-02-18 17:28:23 +0900

    🏷️ Update src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,5 @@ export const hello = (name: string): string => `Hello ${name}`
 export * from './impls'
 export * from './interfaces'
 export * from './types'
+export * from './helpers'
 export * from 'discord.js'
