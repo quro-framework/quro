beb162e58c94636d5a246ec78828781ea3e617c0
hota1024 <hotalog@hota1024.com>
2020-02-18 17:41:28 +0900

    ✅ Add argTypeToString test

---
+++ tests/helpers/argTypeToString.test.ts
@@ -0,0 +1,11 @@
+import { ArgType } from '../../src/types/ArgType'
+
+describe('argTypeToString test', () => {
+  test('ArgType to string', () => {
+    expect(argTypeToString(ArgType.Any)).toBe('any')
+    expect(argTypeToString(ArgType.String)).toBe('string')
+    expect(argTypeToString(ArgType.Number)).toBe('number')
+    expect(argTypeToString(ArgType.Boolean)).toBe('boolean')
+    expect(argTypeToString(-Infinity as ArgType)).toBe('unknown')
+  })
+})
