5d0fd20fa70cf9fa47aec126d081cdd28b308e20
hota1024 <hotalog@hota1024.com>
2020-02-18 16:26:22 +0900

    🏷️ Add ArgDefInterface

---
+++ src/interfaces/ArgDef/index.ts
@@ -0,0 +1,52 @@
+import { ArgType } from '../../types/ArgType'
+import { ArgTypeMap } from '../../types/ArgTypeMap'
+
+/*
+ * ArgDefInterface.
+ */
+export interface ArgDefInterface<
+  Type extends ArgType,
+  ValueType extends ArgTypeMap[Type]
+> {
+  /**
+   * Typed value(Use type only)
+   */
+  typedValue: ValueType
+
+  /**
+   * Display name.
+   */
+  name: string
+
+  /**
+   * Argument type.
+   */
+  type: ArgType
+
+  /**
+   * Default value.
+   */
+  defaultValue?: ValueType
+
+  /**
+   * Excepted values.
+   */
+  exceptedValues: ValueType[]
+
+  /**
+   * Example value.
+   */
+  exampleValue?: ValueType
+
+  /**
+   * Description.
+   */
+  description: string
+
+  /**
+   * Validate value.
+   *
+   * @param value
+   */
+  validate<T>(value: T): boolean
+}
