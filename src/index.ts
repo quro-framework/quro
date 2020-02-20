97abef2158f9ae31a0b881118fe4c9eaf9b148cf
hota1024 <hotalog@hota1024.com>
2020-02-20 19:07:16 +0900

    Update src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,3 @@
-/**
- * Hello.
- *
- * @param name
- */
-export const hello = (name: string): string => `Hello ${name}`
-
 export * from './impls'
 export * from './interfaces'
 export * from './types'
