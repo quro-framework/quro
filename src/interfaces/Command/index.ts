1b4ee7bc07b127e9484c78fa99d869bdc1d051ec
hota1024 <hotalog@hota1024.com>
2020-02-21 00:03:10 +0900

    🏷️ Update CommandInterface

--- src/interfaces/Command/index.ts
+++ src/interfaces/Command/index.ts
@@ -3,11 +3,45 @@ import { CommandResponseInterface } from '../CommandResponse'
 import { PipeNextInterface } from '../PipeNext'
 import { CommandRequestInterface } from '../CommandRequest'
 import { ComponentInterface } from '../Component'
+import { ArgDefInterface } from '../ArgDef'
+import { ArgType } from '../../types/ArgType'
 
 /*
  * CommandInterface.
  */
 export interface CommandInterface extends ComponentInterface {
+  /**
+   * Type check.
+   */
+  typeCheck: boolean
+
+  /**
+   * Arguments Definitions.
+   */
+  argDefs: {
+    [name: string]: ArgDefInterface<ArgType.Any, any>
+  }
+
+  /**
+   * Command name.
+   */
+  name: string
+
+  /**
+   * Aliases of command name.
+   */
+  aliases: string[]
+
+  /**
+   * Command description.
+   */
+  description: string
+
+  /**
+   * Example usages.
+   */
+  exampleUsages: string[]
+
   /**
    * Call on handled.
    */
