e16d2e2345b22f33f45c66ad3e08fd574ab557e0
hota1024 <hotalog@hota1024.com>
2020-02-25 17:15:15 +0900

    ✅ Add CommandFileLoader test

---
+++ tests/CommandFileLoader.test.ts
@@ -0,0 +1,15 @@
+import { CommandFileLoader, isCommand } from '../src'
+import * as path from 'path'
+
+describe('CommandFileLoader test', () => {
+  it('Import commands', done => {
+    const loader = new CommandFileLoader()
+    loader
+      .load(path.resolve(__dirname, './commandFileLoaderCases/ImportCommands'))
+      .then(commands => {
+        expect(commands.every(isCommand)).toBeTruthy()
+        expect(commands.length).toBe(4)
+      })
+      .then(done)
+  })
+})
