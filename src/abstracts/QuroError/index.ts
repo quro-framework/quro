c5ad9dec3975ecdb0b52c37007c7ded3f7faf800
hota1024 <hotalog@hota1024.com>
2020-02-26 17:03:46 +0900

    🏷️ Update QuroError class

--- src/abstracts/QuroError/index.ts
+++ src/abstracts/QuroError/index.ts
@@ -1,4 +1,26 @@
 /*
  * QuroError class.
  */
-export class QuroError extends Error {}
+export class QuroError<DataType = any> extends Error {
+  /**
+   * Error label.
+   */
+  label = ''
+
+  /**
+   * Data.
+   */
+  data: DataType
+
+  /**
+   * QuroError constructor.
+   *
+   * @param label
+   * @param data
+   */
+  constructor(message = '', label = '', data: DataType = {} as any) {
+    super(message)
+    this.label = label
+    this.data = data
+  }
+}
