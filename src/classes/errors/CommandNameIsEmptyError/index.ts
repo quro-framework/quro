091e30fab06d01a3c52e2c1bb1f837fbb08dbec6
hota1024 <hotalog@hota1024.com>
2020-02-28 20:35:18 +0900

    🏷️ Update CommandNameIsEmptyError class

--- src/classes/errors/CommandNameIsEmptyError/index.ts
+++ src/classes/errors/CommandNameIsEmptyError/index.ts
@@ -1,15 +1,28 @@
 import { QuroError } from '../../..'
+import { Message } from 'discord.js'
+
+/**
+ * CommandNameIsEmptyErrorData type.
+ */
+export type CommandNameIsEmptyErrorData = {
+  /**
+   * Message.
+   */
+  message: Message
+}
 
 /*
  * CommandNameIsEmptyError class.
  */
-export class CommandNameIsEmptyError extends QuroError {
+export class CommandNameIsEmptyError extends QuroError<
+  CommandNameIsEmptyErrorData
+> {
   /**
    * CommandNameIsEmptyError constructor.
    *
    * @param message
    */
-  constructor(message: string) {
-    super(message, 'command_name_is_empty')
+  constructor(message: string, data: CommandNameIsEmptyErrorData) {
+    super(message, 'command_name_is_empty', data)
   }
 }
